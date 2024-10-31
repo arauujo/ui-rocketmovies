dd094ec3c8b6ee36b2e865c0b1e51a65b24dd3f6
arauujo <wesleey988@gmail.com>
2024-10-30 21:06:25 -0300

    adjusted text and title color

--- src/components/SweetAlert/styles.js
+++ src/components/SweetAlert/styles.js
@@ -4,4 +4,8 @@ export const CustomSweetAlertStyles = createGlobalStyle`
   .confirm-button {
     color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
   }
+
+  .swal2-title, .swal2-html-container {
+    color: #E1E1E1;
+  }
 `;
